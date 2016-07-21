db324e79b869bcd099bc7edd226ec5beb4f19939
nicememedude <nicememe@my.smccd.edu>
2016-07-21 04:56:55 -0500

    php5enmod

--- lamp.sh
+++ lamp.sh
@@ -9,5 +9,6 @@ sudo echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
 sudo >/var/www/html/index.php
 sudo wget -O /var/www/html/index.php https://raw.githubusercontent.com/nicememedude/autobash/master/noneedtodl/indexlamp.php
 sudo wget -O /etc/apache2/mods-enabled/dir.conf https://raw.githubusercontent.com/nicememedude/autobash/master/noneedtodl/dirlamp.conf
+sudo php5enmod mcrypt
 sudo service apache2 restart
 
