818b983d6398dbb8eebf51d68373cb0c87b78c08
nicememedude <nicememe@my.smccd.edu>
2016-07-21 00:55:51 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo rand(1, 10000);
+echo "\n";
+echo "\n";
+echo "<- If there is no integer between 1 and 10000, something is very wrong!";
+?>
+<p>Random HTML paragraph!</p>
+<a href = "/phpmyadmin">Click here to go to your phpMyAdmin panel.</a>
