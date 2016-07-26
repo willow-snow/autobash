57fb61e892217a6db306be924952a593936dd190
nicememedude <nicememe@my.smccd.edu>
2016-07-26 00:15:32 -0500

    make less noisy, add some dumb comments

--- lamp.sh
+++ lamp.sh
@@ -1,7 +1,9 @@
 #uncomment next line if using root ssh key
 #sudo su
-sudo apt-get update
-sudo apt-get install -y apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt phpmyadmin
+sudo echo "First, we update apt-get repos"
+sudo apt-get -qq update
+sudo echo "Then, we install all the necessary components of a LAMP stack!"
+sudo apt-get -qq install -y apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt phpmyadmin
 sudo mysql_secure_installation
 sudo rm /etc/apache2/mods-enabled/dir.conf
 sudo chmod 777 -R /var/www/
