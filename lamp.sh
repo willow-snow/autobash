603630a0713be8de992ace125b79c1f12809ef37
nicememedude <nicememe@my.smccd.edu>
2016-07-29 00:43:28 -0500

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -9,7 +9,7 @@ sudo rm /etc/apache2/mods-enabled/dir.conf
 sudo chmod 777 -R /var/www/
 sudo echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
 sudo >/var/www/html/index.php
-sudo wget -O /var/www/html/index.php https://raw.githubusercontent.com/nicememedude/autobash/master/noneedtodl/indexlamp.php
+sudo wget -O /var/www/html/index.php https://raw.githubusercontent.com/nicememedude/autobashfiles/master/indexlamp.php
 sudo wget -O /etc/apache2/mods-enabled/dir.conf https://raw.githubusercontent.com/nicememedude/autobash/master/noneedtodl/dirlamp.conf
 sudo php5enmod mcrypt
 sudo service apache2 restart
